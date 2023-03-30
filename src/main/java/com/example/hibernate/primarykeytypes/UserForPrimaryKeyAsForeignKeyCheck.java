0e8181118c02fed60fe4cbd51237daf0649463a4
Sergey Deev <mygrayroom@gmail.com>
2023-03-30 08:38:41 +0600

    Refactoring

--- src/main/java/com/example/hibernate/primarykeytypes/UserForPrimaryKeyAsForeignKeyCheck.java
+++ src/main/java/com/example/hibernate/primarykeytypes/UserForPrimaryKeyAsForeignKeyCheck.java
@@ -1,13 +1,8 @@
 package com.example.hibernate.primarykeytypes;
 
-import com.example.hibernate.entity.Company;
-import com.example.hibernate.entity.PersonalInfo;
-import com.example.hibernate.entity.Role;
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-import org.hibernate.annotations.Type;
 
 @Data
 @NoArgsConstructor
@@ -23,19 +18,6 @@ public class UserForPrimaryKeyAsForeignKeyCheck {
 
     @Column(unique = true)
     String username;
-
-    @Embedded
-    PersonalInfo personalInfo;
-
-    @Enumerated(EnumType.STRING)
-    Role role;
-
-    @Type(JsonBinaryType.class)
-    String info;
-
-    @ManyToOne
-    @JoinColumn(name = "company_id")
-    Company company;
 
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
