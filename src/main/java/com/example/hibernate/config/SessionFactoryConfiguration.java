d6d3c97c3f382b4ed50061214606f738fa8b58c2
Sergey Deev <mygrayroom@gmail.com>
2023-04-13 09:39:28 +0600

    Bug fix

--- src/main/java/com/example/hibernate/config/SessionFactoryConfiguration.java
+++ src/main/java/com/example/hibernate/config/SessionFactoryConfiguration.java
@@ -1,15 +1,11 @@
 package com.example.hibernate.config;
 
 import com.example.hibernate.converter.BirthdayConverter;
-import com.example.hibernate.listeners.event_listener.AuditTableEventListener;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import lombok.experimental.UtilityClass;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.event.service.spi.EventListenerRegistry;
-import org.hibernate.event.spi.EventType;
-import org.hibernate.internal.SessionFactoryImpl;
 
 @UtilityClass
 public class SessionFactoryConfiguration {
@@ -21,12 +17,6 @@ public class SessionFactoryConfiguration {
 //        configuration.setInterceptor(new GlobalInterceptor());
         configuration.configure();
 
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        SessionFactoryImpl sessionFactoryImpl = sessionFactory.unwrap(SessionFactoryImpl.class);
-        EventListenerRegistry eventListenerRegistry = sessionFactoryImpl.getServiceRegistry().getService(EventListenerRegistry.class);
-        eventListenerRegistry.appendListeners(EventType.PRE_INSERT, new AuditTableEventListener());
-        eventListenerRegistry.appendListeners(EventType.PRE_DELETE, new AuditTableEventListener());
-
-        return sessionFactory;
+        return configuration.buildSessionFactory();
     }
 }
--- src/test/java/com/example/hibernate/listeners/event_listener/EventListenerIT.java
+++ src/test/java/com/example/hibernate/listeners/event_listener/EventListenerIT.java
@@ -1,7 +1,19 @@
 package com.example.hibernate.listeners.event_listener;
 
 import com.example.hibernate.BaseIT;
+import com.example.hibernate.config.SessionFactoryConfiguration;
+import com.example.hibernate.converter.BirthdayConverter;
 import com.example.hibernate.entity.User;
+import com.example.hibernate.helpers.MigrationHelper;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.event.service.spi.EventListenerRegistry;
+import org.hibernate.event.spi.EventType;
+import org.hibernate.internal.SessionFactoryImpl;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.example.hibernate.listeners.event_listener.Audit.Operation.DELETE;
@@ -9,6 +21,37 @@ import static com.example.hibernate.listeners.event_listener.Audit.Operation.INS
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class EventListenerIT extends BaseIT {
+
+    private static SessionFactory sessionFactory;
+
+    @BeforeAll
+    static void initSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+        configuration.addAttributeConverter(BirthdayConverter.class, true);
+        configuration.registerTypeOverride(new JsonBinaryType(), new String[]{JsonBinaryType.INSTANCE.getName()});
+        configuration.configure();
+
+        SessionFactory factory = configuration.buildSessionFactory();
+        SessionFactoryImpl sessionFactoryImpl = factory.unwrap(SessionFactoryImpl.class);
+        EventListenerRegistry eventListenerRegistry = sessionFactoryImpl.getServiceRegistry().getService(EventListenerRegistry.class);
+        eventListenerRegistry.appendListeners(EventType.PRE_INSERT, new AuditTableEventListener());
+        eventListenerRegistry.appendListeners(EventType.PRE_DELETE, new AuditTableEventListener());
+
+        sessionFactory = factory;
+    }
+
+    @BeforeAll
+    public static void initDbAndSessionFactory() {
+        MigrationHelper.populateDb(POSTGRES.getJdbcUrl(), POSTGRES.getUsername(), POSTGRES.getPassword());
+
+        sessionFactory = SessionFactoryConfiguration.buildSessionFactory();
+    }
+
+    @AfterAll
+    public static void closeSessionFactory() {
+        sessionFactory.close();
+    }
 
     @Test
     void onPreInsert() {
